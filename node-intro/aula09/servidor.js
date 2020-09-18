855c143f05fd2fcdf75e964716a6d8dfb91d7258
Pedro Henrique <henriquevic012@gmail.com>
2020-09-17 21:56:20 -0300

    Arquivos da aula 09

--- node-intro/aula09/servidor.js
+++ node-intro/aula09/servidor.js
@@ -4,5 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Olá você está na rota home'));
 app.get('/users', (req, res) => res.send('Olá você está na lista de usuários'));
+app.get('/Sobre', (req, res) => res.send('Olá você está página sobre'));
 
 app.listen(porta, () => console.log('Servidor rodando na porta ' + porta));
